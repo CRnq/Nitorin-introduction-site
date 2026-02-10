524f5e564bb121534905f534937191e6f1c677b5
CRnq <0611yurayura@gmail.com>
2026-02-10 18:28:56 +0900

    remove: main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { animate, svg } from "https://esm.sh/animejs";
-
-animate(svg.createDrawable("path"), {
-  draw: ["0 0", "0 1", "1 1"],
-  ease: "inOut(3)",
-  duration: 5000,
-  loop: true,
-});
